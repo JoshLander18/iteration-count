86e6267252fe1e65097e908a12b195123aadf48f
Joshua Watson <102503750+JoshLander18@users.noreply.github.com>
2022-06-25 01:36:39 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+    // initialize the count as 0
+    var i = 0;
+
+    //when button is clicked function
+    function increment() {
+    
+    //increment the count variable when the button i clicked
+    i++;
+
+    //change the count-el in the HTML to reflect the count
+    document.getElementById("count-el").innerHTML = i;
+}
